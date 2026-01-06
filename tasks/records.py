de82cd39bc8efe48dea64fcbdf416c41332ea549
Brian Kelly <brian@spilth.org>
2026-01-06 10:11:14 -0600

    Extract repetitive code that initializes and commits files

--- tasks/records.py
+++ tasks/records.py
@@ -11,6 +11,49 @@ from invoke import task
 from tasks.helpers import json_headers, octet_stream_headers, minimal_record
 
 
+def initialize_and_commit_file(config, draft_id, file_path):
+    print("Uploading file {0}...".format(file_path))
+    file_name = file_path.split("/")[-1]
+    file_data = [{"key": file_name}]
+
+    print("Initializing file...")
+    initialize_file_response = requests.post(
+        "{0}/api/records/{1}/draft/files".format(config["BASE_URL"], draft_id),
+        headers=json_headers(config["BEARER_TOKEN"]),
+        json=file_data,
+        verify=False,
+    )
+
+    print("Initialize File Response Code: {0}".format(initialize_file_response.status_code))
+
+    initialize_json = json.loads(initialize_file_response.content)
+    file_content_url = initialize_json["entries"][0]["links"]["content"]
+    file_commit_url = initialize_json["entries"][0]["links"]["commit"]
+
+    print("File Content URL: {0}".format(file_content_url))
+
+    print("Uploading file...")
+    with open(file_path, "rb") as file:
+        file_upload_response = requests.put(
+            file_content_url,
+            headers=octet_stream_headers(config["BEARER_TOKEN"]),
+            data=file,
+            stream=True,
+            verify=False
+        )
+
+    print("File Upload Response Code: {0}".format(file_upload_response.status_code))
+
+    print("Committing file...")
+    commit_response = requests.post(
+        file_commit_url,
+        headers=json_headers(config["BEARER_TOKEN"]),
+        verify=False,
+    )
+    print("Commit File Response Code: {0}".format(commit_response.status_code))
+    print("")
+
+
 @task(
     help={
         "environment": "Target UltraViolet environment",
@@ -67,44 +110,7 @@ def upload_file(_ctx, draft_id, file_path, environment="local"):
     if os.path.isfile(environment_file):
         config = dotenv_values(environment_file)
 
-        file_name = file_path.split("/")[-1]
-        file_data = [{"key": file_name}]
-
-        print("Initializing file...")
-        initialize_file_response = requests.post(
-            "{0}/api/records/{1}/draft/files".format(config["BASE_URL"], draft_id),
-            headers=json_headers(config["BEARER_TOKEN"]),
-            json=file_data,
-            verify=False,
-        )
-
-        print("Initialize File Response Code: {0}".format(initialize_file_response.status_code))
-
-        initialize_json = json.loads(initialize_file_response.content)
-        file_content_url = initialize_json["entries"][0]["links"]["content"]
-        file_commit_url = initialize_json["entries"][0]["links"]["commit"]
-
-        print("File Content URL: {0}".format(file_content_url))
-
-        print("Uploading file...")
-        with open(file_path, "rb") as file:
-            file_upload_response = requests.put(
-                file_content_url,
-                headers=octet_stream_headers(config["BEARER_TOKEN"]),
-                data=file,
-                stream=True,
-                verify=False
-            )
-
-        print("File Upload Response Code: {0}".format(file_upload_response.status_code))
-
-        print("Committing file...")
-        commit_response = requests.post(
-            file_commit_url,
-            headers=(json_headers(config["BEARER_TOKEN"])),
-            verify=False,
-        )
-        print("Commit File Response Code: {0}".format(commit_response.status_code))
+        initialize_and_commit_file(config, draft_id, file_path)
 
 
 @task(
@@ -127,46 +133,7 @@ def upload_files(_ctx, draft_id, glob_pattern, environment="local"):
         config = dotenv_values(environment_file)
 
         for file_path in glob.glob(glob_pattern):
-            print("Uploading file {0}...".format(file_path))
-            file_name = file_path.split("/")[-1]
-            file_data = [{"key": file_name}]
-
-            print("Initializing file...")
-            initialize_file_response = requests.post(
-                "{0}/api/records/{1}/draft/files".format(config["BASE_URL"], draft_id),
-                headers=json_headers(config["BEARER_TOKEN"]),
-                json=file_data,
-                verify=False,
-            )
-
-            print("Initialize File Response Code: {0}".format(initialize_file_response.status_code))
-
-            initialize_json = json.loads(initialize_file_response.content)
-            file_content_url = initialize_json["entries"][0]["links"]["content"]
-            file_commit_url = initialize_json["entries"][0]["links"]["commit"]
-
-            print("File Content URL: {0}".format(file_content_url))
-
-            print("Uploading file...")
-            with open(file_path, "rb") as file:
-                file_upload_response = requests.put(
-                    file_content_url,
-                    headers=octet_stream_headers(config["BEARER_TOKEN"]),
-                    data=file,
-                    stream=True,
-                    verify=False
-                )
-
-            print("File Upload Response Code: {0}".format(file_upload_response.status_code))
-
-            print("Committing file...")
-            commit_response = requests.post(
-                file_commit_url,
-                headers=json_headers(config["BEARER_TOKEN"]),
-                verify=False,
-            )
-            print("Commit File Response Code: {0}".format(commit_response.status_code))
-            print("")
+            initialize_and_commit_file(config, draft_id, file_path)
 
 
 @contextmanager
