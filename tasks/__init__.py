05ec90ba51b00f377cabbac15e92c61e06d24409
Brian Kelly <brian@spilth.org>
2026-01-14 09:22:03 -0600

    Adds communities tasks for listing, viewing and enabling subcommunities

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -1,6 +1,7 @@
 from invoke import Collection
 
-from tasks import records, core
+from tasks import records, core, communities
 
 ns = Collection(initialize=core.initialize)
 ns.add_collection(records)
+ns.add_collection(communities)
